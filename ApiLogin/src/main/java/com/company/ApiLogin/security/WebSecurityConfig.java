c9dade7199f359f506da05ce656fbf451639c492
Murad <muradteymurov@sinam.net>
2023-11-01 18:41:18 +0400

    sbc3

--- ApiLogin/src/main/java/com/company/ApiLogin/security/WebSecurityConfig.java
+++ ApiLogin/src/main/java/com/company/ApiLogin/security/WebSecurityConfig.java
@@ -7,32 +7,73 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    @Bean
-    public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
+//    @Bean
+//    public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
+//
+//        http
+//                .cors()
+//                .disable()
+//                .csrf()
+//                .disable()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .formLogin()
+//                .disable()
+//                .securityMatcher("/**")
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/")
+//                        .permitAll()
+//                        .anyRequest()
+//                        .authenticated()
+//                );
+//
+//        return http.build();
+//    }
 
+//    @Bean
+//    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/register/**").permitAll())
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers("/users/**").hasAnyAuthority("USER","ADMIN"))
+//                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+//                .formLogin(withDefaults())
+//                .httpBasic(withDefaults());
+//        return http.build();
+//    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .disable()
-                .csrf()
-                .disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .formLogin()
-                .disable()
-                .securityMatcher("/**")
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/")
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated()
-                );
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form.disable()
+                )
+                .logout((logout) -> logout.permitAll());
 
         return http.build();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
 
